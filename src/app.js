2596180c83e5ec29d594b0356a01a29257c60419
Tanush <87197372+tanushmandha@users.noreply.github.com>
2021-09-17 19:58:26 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,59 @@
+//Trial 1:
+//We've got some basic info about Karen's home
+//Debug the type of data provided
+//Return the types concatenated in a single variable
+function moreAboutHome(addres,distanceFromHome,hasNeighbours){
+    return (typeof(addres)+ typeof(distanceFromHome) + typeof(hasNeighbours))
+}
+//Trial 2:
+//Check if the data given is of the right type
+//parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents,noOfsiblings,isNuclearFamily){
+    if(typeof(parents) == "string" && typeof(noOfsiblings) == "number" && typeof(isNuclearFamily) =="boolean")
+        return true
+     else
+        return false  
+}
+//Trial 3:
+//Lily is suspicious about Karen's new friend
+//Karen tells her friend's age and even writes it down
+//Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText,ageInNumber){
+    if(isNaN(ageInNumber))
+        return ageInNumber
+    else 
+        if(isNaN(ageInText))
+        return ageInText
+}
+//Trial 4:
+//Lily gave Karen x sweets
+//Karen ate y sweets herself
+//On her way to the river, she ate another z sweets every n meters travelled
+//Her friend divided the remaining sweets into 2 parts for each
+//How many sweets did her friend get to eat?
+//x-(y+z*n)/2
+
+function sweetTooth(totalNoOfSweets,sweetsConsumedByKaren,SweetsConsumedInNMeters,metersToTravel){
+    var remaining = (totalNoOfSweets - (sweetsConsumedByKaren + SweetsConsumedInNMeters * metersToTravel)) /2
+    return remaining 
+}
+//Trial 5:
+//As Lily moves closer, it gets colder. She checks the temperature on her mobile
+//It only shows in fahrenheit. Convert the data to celsius and return it.
+
+//Trial 6:
+//Lily can now do multiple things to deal with this
+//1. Take her daughter to a doctor
+//2. Talk to her husband about it
+//3. Counsel her daughter herself
+//4. Lock her daughter in her room
+//Given a value, return which of these above actions Lily would take
+
+//Challenge 1:  
+//Lily realized that she'd hurt her daughter
+//All she wants now is for her to stop crying
+//She refuses to talk to her but Lily doesn't stop trying
+//She tries out multiple things hoping for the best
+//Take all of Lily's strategies and concatenate them to a single var
+//Seperate the strategies by a single space
+//Return the length of the complete strategy
